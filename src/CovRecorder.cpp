74705a01df43aa8f859c36b78c91859113845e34
Vito Gamberini <vito@gamberini.email>
2023-05-29 03:44:24 -0400

    fix: Fix bad filename bug

--- src/CovRecorder.cpp
+++ src/CovRecorder.cpp
@@ -1,10 +1,12 @@
+#include <algorithm>
+#include <string>
+
 #include <catch2/catch_test_case_info.hpp>
 #include <catch2/reporters/catch_reporter_event_listener.hpp>
 #include <catch2/reporters/catch_reporter_registrars.hpp>
 
 #include <verilated.h>
 #include <verilated_cov.h>
-
 
 class VerilatorCoverageReporter : public Catch::EventListenerBase {
 public:
@@ -16,7 +18,16 @@ public:
 
   void testCaseEnded(const Catch::TestCaseStats& stats) override {
     Verilated::threadContextp()->coveragep()->write(
-        (stats.testInfo->name + ".dat").data());
+        makeFileName(stats.testInfo->name).data());
+  }
+
+private:
+  std::string makeFileName(const std::string& testName) {
+    std::string fileName(testName.size(), '\0');
+    std::string unsafe {"/<>:\"\\|?*"};
+    std::transform(testName.begin(), testName.end(), fileName.begin(),
+        [&](char c) { return unsafe.find(c) == std::string::npos ? c : '_'; });
+    return fileName + ".dat";
   }
 };
 
