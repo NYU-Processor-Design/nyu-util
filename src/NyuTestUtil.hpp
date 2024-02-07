e9eb1df0ba98cb6a58a5126d4db29a0bf278dbd7
Vito Gamberini <vito@gamberini.email>
2024-02-07 03:10:52 -0500

    refactor(testutil): Implement transparent tracers
    * Bump version to 1.2.0
    * Tracers are now transparently returned by getDUT() when tracing is enabled
    * Tracers use CRTP to pass-thru DUT variables to tests
    * Tracers use the test name for the trace file if no other name is given
    * nyu::eval/reset/tick are now neibloids, getDUT is still ADL discoverable

--- src/NyuTestUtil.hpp
+++ src/NyuTestUtil.hpp
@@ -4,102 +4,99 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <catch2/interfaces/catch_interfaces_capture.hpp>
+
 #include <verilated.h>
 #include <verilated_fst_c.h>
 
 namespace nyu {
 
-// clang-format off
 template <typename T>
-concept evalable = requires(T a) {
+concept evalable = requires(T a) { a.eval(); };
+
+template <typename T>
+concept tickable = requires(T a) {
   a.eval();
+  a.clk = 0;
 };
 
-template<typename T>
-concept tickable = requires (T a) {
-  a.eval();
-  a.clk;
-};
-
-template<typename T>
+template <typename T>
 concept resetable = requires(T a) {
   a.eval();
-  a.nReset;
+  a.nReset = 0;
 };
 
 template <typename T>
-concept traceable = requires(T a, VerilatedFstC fst) {
-  a.trace(&fst, 1);
-};
+concept traceable = requires(T a, VerilatedFstC fst) { a.trace(&fst, 1); };
 
-template<typename T>
-concept sync_traceable = tickable<T> && resetable<T> && traceable<T>;
-// clang-format on
+template <typename T>
+concept not_traceable = evalable<T> && !traceable<T>;
 
-template <traceable T> struct tracer {
-
-  tracer(T& dut, const char* file, int levels = 99) : dut {dut} {
-    Verilated::traceEverOn(true);
-    dut.trace(&fst, levels);
-    fst.open(file);
+template <traceable T> struct tracer : T {
+  void eval() {
+    T::eval();
+    _fst->dump(_time++);
   }
 
-  T& dut;
-  VerilatedFstC fst;
-  std::uint64_t time {0};
+  ~tracer() {
+    delete _fst;
+  }
+
+  VerilatedFstC* _fst {nullptr};
+  std::uint64_t _time {0};
 };
 
-template <evalable T> void eval(T& dut, std::size_t cycles = 1) {
+constexpr inline auto eval = [](evalable auto& dut, std::size_t cycles = 1) {
   for(size_t i {0}; i < cycles; ++i) {
     dut.eval();
   }
-}
+};
 
-template <traceable T> void eval(tracer<T>& tracer, std::size_t cycles = 1) {
-  for(size_t i {0}; i < cycles; ++i) {
-    tracer.dut.eval();
-    tracer.fst.dump(tracer.time++);
-  }
-}
-
-void tick(tickable auto& dut, std::size_t cycles = 1) {
+constexpr inline auto tick = [](tickable auto& dut, std::size_t cycles = 1) {
   for(size_t i {0}; i < cycles; ++i) {
     dut.clk = 0;
     dut.eval();
     dut.clk = 1;
     dut.eval();
   }
-}
+};
 
-template <sync_traceable T>
-void tick(tracer<T>& tracer, std::size_t cycles = 1) {
-  for(size_t i {0}; i < cycles; ++i) {
-    tracer.dut.clk = 0;
-    eval(tracer);
-    tracer.dut.clk = 1;
-    eval(tracer);
-  }
-}
-
-void reset(resetable auto& dut) {
+constexpr inline auto reset = [](resetable auto& dut) {
   dut.nReset = 1;
   dut.eval();
   dut.nReset = 0;
   dut.eval();
   dut.nReset = 1;
+};
+
+template <traceable T>
+tracer<T>& getDUT(std::string fileName = {}, int levels = 99) {
+  static tracer<T> dut;
+
+  if(fileName.empty()) {
+    std::string testName {Catch::getResultCapture().getCurrentTestName()};
+    std::string unsafe {"/<>:\"\\|?*"};
+
+    fileName.resize(testName.size());
+    std::transform(testName.begin(), testName.end(), fileName.begin(),
+        [&](char c) { return unsafe.find(c) == std::string::npos ? c : '_'; });
+    fileName += ".fst";
+  }
+
+  Verilated::traceEverOn(true);
+
+  delete dut._fst;
+  dut._time = 0;
+  dut._fst = new VerilatedFstC;
+  dut.trace(dut._fst, levels);
+  dut._fst->open(fileName.c_str());
+
+  return dut;
 }
 
-template <sync_traceable T> void reset(tracer<T>& tracer) {
-  tracer.dut.nReset = 1;
-  eval(tracer);
-  tracer.dut.nReset = 0;
-  eval(tracer);
-  tracer.dut.nReset = 1;
-}
-
-template <typename T> T& getDUT() {
-  static T* p {new T};
-  return *p;
+template <not_traceable T> T& getDUT(std::string = {}, int = 0) {
+  static T dut;
+  return dut;
 }
 
 } // namespace nyu
